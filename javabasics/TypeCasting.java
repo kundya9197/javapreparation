ea1089a7d031403a180f432a84acbe9a42afc4ad
kundan.cdat@gmail.com <kundanmahajan18@gmail.com>
2026-05-08 19:50:58 +0530

    type casting and conversion

---
+++ javabasics/TypeCasting.java
@@ -0,0 +1,13 @@
+public class TypeCasting {
+    public static void main(String args[]) {
+        // type casting
+        int marks = 97;
+        double totalMarks = marks;
+
+        // type conversion
+        float percentage = 67.40f;
+        int finalpercentage = (int) percentage;
+        System.out.println(totalMarks);
+        System.out.println(finalpercentage);
+    }
+}
