292315cd5439b56f674528b1c481d8cdbfa36a86
kundan.cdat@gmail.com <kundanmahajan18@gmail.com>
2026-05-11 01:07:40 +0530

    multiplication using for loop

---
+++ javabasics/ControlFlowForLoop.java
@@ -0,0 +1,12 @@
+public class ControlFlowForLoop {
+    public static void main(String args[]) {
+        // for (int i = 1; i <= 10; i++) {
+        // System.out.println("Current number is " + i);
+        // }
+
+        // multiplication table using for loop
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(2 + " * " + i + " = " + 2 * i);
+        }
+    }
+}
